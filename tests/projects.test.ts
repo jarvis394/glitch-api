c6cd177284e11b949d93ce5ba50e99c649d272f7
jarvis394 <vlad.ekushev@mail.ru>
2019-12-21 16:20:30 +0300

    tests: fix tests fail because of fetch error

--- tests/projects.test.ts
+++ tests/projects.test.ts
@@ -3,7 +3,7 @@ import { Glitch } from '../src'
 const glitch = new Glitch()
 const { api } = glitch
 const ID = '016af9e7-d077-4854-83f3-b0863c88ba35'
-const DOMAIN = 'glapi'
+const DOMAIN = 'community'
 
 describe('Get projects', () => {
   it('should get project by id', async () => {
@@ -14,7 +14,7 @@ describe('Get projects', () => {
 
   it('should get project by domain', async () => {
     const res = await api.projects.get({ domain: DOMAIN })
-
+    
     expect(res.domain).toBe(DOMAIN)
   })
 })
@@ -24,5 +24,5 @@ describe('Search project', () => {
     const res = await api.projects.search({ q: DOMAIN })
 
     expect(res).not.toBeNull()
-  })
+  }, 60 * 1000)
 })
